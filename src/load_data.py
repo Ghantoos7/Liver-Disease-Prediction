5df257d038242e41ced933e89274ac835444ee8d
Ghantoos7 <ghnatios1@gmail.com>
2024-03-05 22:38:21 +0200

    Loading & Exploring <> Fixed issue with loading data

--- src/load_data.py
+++ src/load_data.py
@@ -1,28 +1,18 @@
 import openml
 import pandas as pd
-from scipy.io import arff
-
-
 
 def download_openml_dataset(dataset_id):
-
     """
     Download a dataset from OpenML and return it as a pandas DataFrame.
-
     Parameters:
     - dataset_id: The ID of the dataset to be downloaded.
 
     Returns:
     - A DataFrame containing the downloaded dataset.
     """
+    dataset = openml.datasets.get_dataset(dataset_id, download_data=True)
+    X, y, attribute_names, _ = dataset.get_data(target='Class', dataset_format='dataframe')
+    df = pd.concat([X, y], axis=1)
 
-    dataset = openml.datasets.get_dataset(dataset_id)
-    data, _, _, _ = dataset.get_data(target=dataset.default_target_attribute)
-
-    if isinstance(data, pd.DataFrame):
-        df = data
-    else:
-        data_arff = arff.loadarff(data)
-        df = pd.DataFrame(data_arff[0])
-    
     return df
+
